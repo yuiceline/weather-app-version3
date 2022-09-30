b5f4cdb42c2ef817541b7273e7866def1f9d85ca
yuiceline <113922020+yuiceline@users.noreply.github.com>
2022-09-30 14:31:18 +0900

    Added weather icon

--- src/script.js
+++ src/script.js
@@ -55,6 +55,12 @@ function showTemperature(response) {
 
   let currentWind = document.querySelector("#current-wind");
   currentWind.innerHTML = Math.round(response.data.wind.speed);
+
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function search(city) {
